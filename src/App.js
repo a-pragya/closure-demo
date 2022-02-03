1ca617c919c948888a1b74c29318354b13b79ca3
pragya <pragyaarora13@gmail.com>
2022-02-03 21:29:41 +0530

    update useCallback dependency

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useCallback } from "react";
+import Button from "./components/ButtonComponent";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(true);
+  const [enableToggle, setEnableToggle] = useState(false);
+
+  const enableToggleHandler = useCallback(() => {
+    setEnableToggle(true);
+  }, []);
+
+  // Using the following function, paragraph should be toggled if enableToggle is true.
+  //However since toggleShowParagraph function forms a closure, the value of 'enableToggle' set to false.
+  //Since useCallback hook does not have any dependencies, toggleShowParagraph function will not be recreated
+  //and 'enableToggle' will remain false in toggleShowParagraph
+
+  // const toggleShowParagraph = useCallback(() => {
+  //   if (enableToggle) setShowParagraph((prevState) => !prevState);
+  // }, []);
+
+  //the following function has enableToggle as a dependency, hence it will work correctly
+
+  const toggleShowParagraph = useCallback(() => {
+    if (enableToggle) setShowParagraph((prevState) => !prevState);
+  }, [enableToggle]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Closure Demo</h1>
+      <Button onClick={enableToggleHandler}>Enable Toggle</Button>
+      <p>{showParagraph ? "Hello World" : ""}</p>
+      <Button onClick={toggleShowParagraph}>Toggle Show Paragraph</Button>
     </div>
   );
 }
